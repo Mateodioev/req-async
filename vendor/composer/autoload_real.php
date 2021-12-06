88095635d5ba4c2a9bf5fd238d6dc5237075fcf3
Mateo Ore <orehurcomito@gmail.com>
2021-12-05 22:59:48 -0500

    modified composer.json

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit671970e57fcb4c5cacb70a4ef5a8b7bd
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit671970e57fcb4c5cacb70a4ef5a8b7bd', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit671970e57fcb4c5cacb70a4ef5a8b7bd', 'loadClassLoader'));
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,26 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'reference' => NULL,
-        'name' => 'mateodioev/kirari',
+        'aliases' => array(
+            0 => '1.1.x-dev',
+        ),
+        'reference' => '359e086012e880f9d5f742fbfd3bc48375133e73',
+        'name' => 'mateodioev/async-curl-requests',
         'dev' => true,
     ),
     'versions' => array(
-        'mateodioev/kirari' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+        'mateodioev/async-curl-requests' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'reference' => NULL,
+            'aliases' => array(
+                0 => '1.1.x-dev',
+            ),
+            'reference' => '359e086012e880f9d5f742fbfd3bc48375133e73',
             'dev_requirement' => false,
         ),
     ),
